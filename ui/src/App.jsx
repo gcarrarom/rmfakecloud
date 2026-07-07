e2b17381402bd6fdf611b0f62df0c75fdfcc5bd5
Gui Martins <gui.martins@fancywhale.ca>
2026-07-07 00:12:00 -0400

    fix: renderer

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -25,10 +25,6 @@ import "./App.scss"
 
 import { pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.js';
-pdfjs.GlobalWorkerOptions.workerPort = new Worker(
-  new URL('/pdf.worker.js', window.location.href),
-  { type: 'module' }
-);
 
 export default function App() {
 
